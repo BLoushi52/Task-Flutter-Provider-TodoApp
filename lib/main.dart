867ba9bf300bb699f691a4e829b4b725a4643073
BLoushi52 <k.albloushii@hotmail.com>
2022-11-22 19:32:58 +0300

    commented unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
-import 'package:todo_app/pages/add_page.dart';
-import 'package:todo_app/pages/home_page.dart';
+// import 'package:todo_app/pages/add_page.dart';
+// import 'package:todo_app/pages/home_page.dart';
 import 'package:todo_app/providers/task_provider.dart';
 
 import 'pages/add_task_page.dart';
--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+// import 'package:flutter/src/foundation/key.dart';
+// import 'package:flutter/src/widgets/container.dart';
+// import 'package:flutter/src/widgets/framework.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/providers/task_provider.dart';
